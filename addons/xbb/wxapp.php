b7d78bdac6eb73495ffd6780c52ab0976052c3a9
wujiepeng <517987404@qq.com>
2019-09-02 14:12:45 +0800

    接口信息

--- addons/xbb/wxapp.php
+++ addons/xbb/wxapp.php
@@ -4,24 +4,23 @@ defined('IN_IA') or exit('Access Denied');
 
 class XbbModuleWxapp extends WeModuleWxapp
 {
-
-    public function doPageCode()
-    {
-        global $_GPC, $_W;
-        $url = "https://api.weixin.qq.com/sns/jscode2session?appid={$_W['account']['key']}&secret={$_W['account']['secret']}&js_code={$_GPC['code']}&grant_type=authorization_code";
-        $result = file_get_contents($url);
-        $result = json_decode($result, true);
-        $data = $result;
-        $this->result(0, '获取openid', $data);
-    }
+//    public function doPageCode()
+//    {
+//        global $_GPC, $_W;
+//        $url = "https://api.weixin.qq.com/sns/jscode2session?appid={$_W['account']['key']}&secret={$_W['account']['secret']}&js_code={$_GPC['code']}&grant_type=authorization_code";
+//        $result = file_get_contents($url);
+//        $result = json_decode($result, true);
+//        $data = $result;
+//        $this->result(0, '获取openid', $data);
+//    }
 
     /**
-     * 首页幻灯片
+     * 首页轮播图片
      */
     public function doPagePicture()
     {
         global $_GPC, $_W;
-        $picture = pdo_getall('lv_suo_pic', ['uniacid' => $_W['uniacid']]);
+        $picture = pdo_getall('xbb_picture', ['uniacid' => $_W['uniacid']]);
         foreach ($picture as $key => $vo)
         {
             $picture[$key]['imgurl'] = tomedia($vo['imgurl']);
@@ -30,39 +29,57 @@ class XbbModuleWxapp extends WeModuleWxapp
     }
 
     /**
+     * 银行
+     */
+    public function doPageBank()
+    {
+        global $_GPC, $_W;
+        $bank = pdo_getall('xbb_bank', ['uniacid' => $_W['uniacid']]);
+        $this->result(0, '获取成功', $bank);
+    }
+
+    /**
+     * 服务项目
+     */
+    public function doPageProject()
+    {
+        global $_GPC, $_W;
+        $project = pdo_getall('xbb_project', ['uniacid' => $_W['uniacid']]);
+        $this->result(0, '获取成功', $project);
+    }
+
+    /**
      * 优质解答
      */
-    public function doPageAnswer()
-    {
-        global $_GPC, $_W;
-        $sql = "select * from" . tablename('lv_suo_answer') . " where uniacid=" . $_W['uniacid'];
-        $answer = pdo_fetchall($sql);
-        // 分页开始
-        $total = count($answer);
-        $pageindex = max($_GPC['page'], 1);
-        $pagesize = 2;
-        $page = pagination($total, $pageindex, $pagesize);
-        $p = ($pageindex - 1) * 2;
-        $sql .= " order by answerid desc limit " . $p . " , " . $pagesize;
-        $answer = pdo_fetchall($sql);
-        // 分页結束
-        $this->result(0, $total, $answer);
-    }
-
-    public function doPageConsult()
-    {
-        global $_GPC, $_W;
-        $data = [
-            'content' => $_GPC['content'],
-            'phone' => $_GPC['phone'],
-            'addtime' => time(),
-            'uniacid' => $_W['uniacid'],
-        ];
-        $result = pdo_insert('lv_suo_consult', $data);
-        if (!empty($result))
-        {
-            $this->result(0, '提交成功', ['status' => 'success']);
-        }
-    }
-
+//    public function doPageAnswer()
+//    {
+//        global $_GPC, $_W;
+//        $sql = "select * from" . tablename('lv_suo_answer') . " where uniacid=" . $_W['uniacid'];
+//        $answer = pdo_fetchall($sql);
+//        // 分页开始
+//        $total = count($answer);
+//        $pageindex = max($_GPC['page'], 1);
+//        $pagesize = 2;
+//        $page = pagination($total, $pageindex, $pagesize);
+//        $p = ($pageindex - 1) * 2;
+//        $sql .= " order by answerid desc limit " . $p . " , " . $pagesize;
+//        $answer = pdo_fetchall($sql);
+//        // 分页結束
+//        $this->result(0, $total, $answer);
+//    }
+//    public function doPageConsult()
+//    {
+//        global $_GPC, $_W;
+//        $data = [
+//            'content' => $_GPC['content'],
+//            'phone' => $_GPC['phone'],
+//            'addtime' => time(),
+//            'uniacid' => $_W['uniacid'],
+//        ];
+//        $result = pdo_insert('lv_suo_consult', $data);
+//        if (!empty($result))
+//        {
+//            $this->result(0, '提交成功', ['status' => 'success']);
+//        }
+//    }
 }
